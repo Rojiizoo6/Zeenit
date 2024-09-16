a3ff1664de2dee78de4c732d21524d901e863f73
Rojiizoo6 <67375960+Rojiizoo6@users.noreply.github.com>
2024-09-16 12:24:53 -0300

    Add files via upload

---
+++ twitchZeenit/main.py
@@ -0,0 +1,243 @@
+# -*- coding: utf-8 -*-
+
+import logging
+from colorama import Fore
+from TwitchChannelPointsMiner import TwitchChannelPointsMiner
+from TwitchChannelPointsMiner.logger import LoggerSettings, ColorPalette
+from TwitchChannelPointsMiner.classes.Chat import ChatPresence
+from TwitchChannelPointsMiner.classes.Discord import Discord
+from TwitchChannelPointsMiner.classes.Telegram import Telegram
+from TwitchChannelPointsMiner.classes.Matrix import Matrix
+from TwitchChannelPointsMiner.classes.Pushover import Pushover
+from TwitchChannelPointsMiner.classes.Settings import Priority, Events, FollowersOrder
+from TwitchChannelPointsMiner.classes.entities.Bet import Strategy, BetSettings, Condition, OutcomeKeys, FilterCondition, DelayMode
+from TwitchChannelPointsMiner.classes.entities.Streamer import Streamer, StreamerSettings
+from keep_alive import keep_alive
+import os
+
+keep_alive()
+
+twitch_miner = TwitchChannelPointsMiner(
+    username=os.environ.get('username'),
+    password=os.environ.get('password'),           # If no password will be provided, the script will ask interactively
+    claim_drops_startup=True,                  # If you want to auto claim all drops from Twitch inventory on the startup
+    priority=[                                  # Custom priority in this case for example:
+        Priority.STREAK,
+        Priority.SUBSCRIBED,                # - We want first of all to catch all watch streak from all streamers
+        Priority.DROPS,                         # - When we don't have anymore watch streak to catch, wait until all drops are collected over the streamers
+        Priority.ORDER                          # - When we have all of the drops claimed and no watch-streak available, use the order priority (POINTS_ASCENDING, POINTS_DESCEDING)
+    ],
+    enable_analytics=False,                     # Disables Analytics if False. Disabling it significantly reduces memory consumption
+    disable_ssl_cert_verification=False,        # Set to True at your own risk and only to fix SSL: CERTIFICATE_VERIFY_FAILED error
+    disable_at_in_nickname=False,               # Set to True if you want to check for your nickname mentions in the chat even without @ sign
+    logger_settings=LoggerSettings(
+        save=True,                              # If you want to save logs in a file (suggested)
+        console_level=logging.INFO,             # Level of logs - use logging.DEBUG for more info
+        console_username=False,                 # Adds a username to every console log line if True. Also adds it to Telegram, Discord, etc. Useful when you have several accounts
+        auto_clear=True,                        # Create a file rotation handler with interval = 1D and backupCount = 7 if True (default)
+        time_zone="",                           # Set a specific time zone for console and file loggers. Use tz database names. Example: "America/Denver"
+        file_level=logging.DEBUG,               # Level of logs - If you think the log file it's too big, use logging.INFO
+        emoji=True,                             # On Windows, we have a problem printing emoji. Set to false if you have a problem
+        less=False,                             # If you think that the logs are too verbose, set this to True
+        colored=True,                           # If you want to print colored text
+        color_palette=ColorPalette(             # You can also create a custom palette color (for the common message).
+            STREAMER_online="GREEN",            # Don't worry about lower/upper case. The script will parse all the values.
+            streamer_offline="red",             # Read more in README.md
+            BET_wiN=Fore.MAGENTA                # Color allowed are: [BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET].
+        ),
+        telegram=Telegram(                                                          # You can omit or set to None if you don't want to receive updates on Telegram
+            chat_id=123456789,                                                      # Chat ID to send messages @getmyid_bot
+            token="123456789:shfuihreuifheuifhiu34578347",                          # Telegram API token @BotFather
+            events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE,
+                    Events.BET_LOSE, Events.CHAT_MENTION],                          # Only these events will be sent to the chat
+            disable_notification=True,                                              # Revoke the notification (sound/vibration)
+        ),
+        discord=Discord(
+            webhook_api="https://discord.com/api/webhooks/1258435703604379669/zVx8JosGVdQOAUwrgKTZBugXY7aI0Ac4Kl_eiVfwVcV5LTcjvRg9lqLurTSLpXtBy48s",  # Discord Webhook URL
+            events=[Events.GAIN_FOR_RAID,Events.BET_WIN, Events.BET_LOSE,
+                    Events.CHAT_MENTION,Events.GAIN_FOR_CLAIM,Events.GAIN_FOR_WATCH,Events.BONUS_CLAIM
+                    ,Events.MOMENT_CLAIM,Events.JOIN_RAID],                                  # Only these events will be sent to the chat
+        ),
+        matrix=Matrix(
+            username="twitch_miner",                                                   # Matrix username (without homeserver)
+            password="...",                                                            # Matrix password
+            homeserver="matrix.org",                                                   # Matrix homeserver
+            room_id="...",                                                             # Room ID
+            events=[Events.STREAMER_ONLINE, Events.STREAMER_OFFLINE, Events.BET_LOSE], # Only these events will be sent to the chat
+        ),
+        pushover=Pushover(
+            userkey="YOUR-ACCOUNT-TOKEN",                                             # Login to https://pushover.net/, the user token is on the main page.
+            token="YOUR-APPLICATION-TOKEN",                                           # Create a application on the website, and use the token shown in your application.
+            priority=0,                                                               # Read more about priority here: https://pushover.net/api#priority
+            sound="pushover",                                                         # A list of sounds can be found here: https://pushover.net/api#sounds
+            events=[Events.CHAT_MENTION, Events.DROP_CLAIM],                          # Only these events will be sent.
+        )
+    ),
+    streamer_settings=StreamerSettings(
+        make_predictions=True,                  # If you want to Bet / Make prediction
+        follow_raid=True,                       # Follow raid to obtain more points
+        claim_drops=True,                       # We can't filter rewards base on stream. Set to False for skip viewing counter increase and you will never obtain a drop reward from this script. Issue #21
+        claim_moments=True,                     # If set to True, https://help.twitch.tv/s/article/moments will be claimed when available
+        watch_streak=True,                      # If a streamer go online change the priority of streamers array and catch the watch screak. Issue #11
+        chat=ChatPresence.ONLINE,               # Join irc chat to increase watch-time [ALWAYS, NEVER, ONLINE, OFFLINE]
+        bet=BetSettings(
+            strategy=Strategy.MOST_VOTED,            # Choose you strategy!
+            percentage=10,                       # Place the x% of your channel points
+            percentage_gap=10,                  # Gap difference between outcomesA and outcomesB (for SMART strategy)
+            max_points=100000,                   # If the x percentage of your channel points is gt bet_max_points set this value
+            stealth_mode=True,                  # If the calculated amount of channel points is GT the highest bet, place the highest value minus 1-2 points Issue #33
+            delay_mode=DelayMode.FROM_START,      # When placing a bet, we will wait until delay seconds before the end of the timer
+            delay=20,
+            minimum_points=2000,               # Place the bet only if we have at least 20k points. Issue #113
+            filter_condition=FilterCondition(
+                by=OutcomeKeys.ODDS,     # Where apply the filter. Allowed [PERCENTAGE_USERS, ODDS_PERCENTAGE, ODDS, TOP_POINTS, TOTAL_USERS, TOTAL_POINTS]
+                where=Condition.GTE,            # 'by' must be [GT, LT, GTE, LTE] than value
+                value=1
+            )
+        )
+    )
+)
+
+# You can customize the settings for each streamer. If not settings were provided, the script would use the streamer_settings from TwitchChannelPointsMiner.
+# If no streamer_settings are provided in TwitchChannelPointsMiner the script will use default settings.
+# The streamers array can be a String -> username or Streamer instance.
+
+# The settings priority are: settings in mine function, settings in TwitchChannelPointsMiner instance, default settings.
+# For example, if in the mine function you don't provide any value for 'make_prediction' but you have set it on TwitchChannelPointsMiner instance, the script will take the value from here.
+# If you haven't set any value even in the instance the default one will be used
+
+#twitch_miner.analytics(host="127.0.0.1", port=5000, refresh=5, days_ago=7)   # Start the Analytics web-server
+twitch_miner.mine(
+    [
+        "ninitarivers",  # Añade aquí el nombre del streamer con mayor prioridad
+    ],  # Array of streamers (order = priority)
+    followers=True,
+    blacklist=[
+        "suigei_kv",  # Añade aquí el nombre del usuario a ser blacklisted
+        "sawa",  # Añade aquí el nombre del usuario a ser blacklisted
+        "ezeseveri",  # Y así sucesivamente
+        "mamykka",
+        "dr7coo",
+        "theakaleina",
+        "kiravtgames",
+        "luvmails",
+        "panchikofps",
+        "maaakky",
+        "estefania",
+        "yoladragonuwu",
+        "nonevt_",
+        "iammendry",
+        "itsznko",
+        "vaxelleart",
+        "symariana",
+        "bysotito",
+        "valkie_",
+        "loved",
+        "symphonyars",
+        "kumiflumi",
+        "nanavr_",
+        "minigodcs",
+        "hopeluss",
+        "matty_ow",
+        "meguwu_vr",
+        "wolverdash_sup",
+        "stely_69_",
+        "itsblot",
+        "isnedd",
+        "rusjustthat",
+        "sweetsamanthavr",
+        "artsyvrc",
+        "neet",
+        "shepiraa",
+        "emicakiiie",
+        "mousy",
+        "lushstarlight",
+        "vickypalami",
+        "kiuwt",
+        "suwie",
+        "nixprinces",
+        "gingamy",
+        "katyma12",
+        "yanya__",
+        "julinhazsz",
+        "kradiiiiii",
+        "renji_vt",
+        "aeriytheneko",
+        "claudioshaddix",
+        "nananightray",
+        "msskylarvr",
+        "lichbait",
+        "lolztehtroll",
+        "tanjivt",
+        "futurostraders",
+        "discopeligro",
+        "daykaagg",
+        "dashanvrrouwu",
+        "jouffa",
+        "alexavr",
+        "ir9ne",
+        "zayi",
+        "xeawee",
+        "zilvyvt",
+        "scripter1v9",
+        "ronn",
+        "ferdy_val",
+        "michellesadaf",
+        "beau194448",
+        "raketadlo70",
+        "1leaff",
+        "valorant",
+        "valorant_la",
+        "sarah_frags",
+        "melsi_bee",
+        "meryafps",
+        "safiro01",
+        "lilycarries",
+        "elizabeth_vt",
+        "goretwitch",
+        "aconig",
+        "aniboops",
+        "nonechan_",
+        "aspen",
+        "adamaraver",
+        "shyzinha",
+        "melanji",
+        "florescent",
+        "hanaikadq_222",
+        "westsenju",
+        "noiaval",
+        "koda_mai",
+        "araravilano",
+        "coraline_uwu",
+        "xsoprole",
+        "kittyasmr1",
+        "aubrey",
+        "klossfps",
+        "xsuelly",
+        "yaroko",
+        "jaeky",
+        "t3xturekr",
+        "akaskylr",
+        "lannox14",
+        "bnjxh",
+        "nanainemu",
+        "amagiau",
+        "alexis11ninja",
+        "akamesita",
+        "cartoon",
+        "akahana_mel",
+        "zeeniiit",
+        "nategentile7",
+        "xuiqtz",
+        "daikaw",
+        "soez3",
+        "minminuu_",
+        "nicoolee03",
+        "xsmurfinha",
+        "happitox",
+        "ludilue",
+        "officialboaster",
+        "imgisela",
+    ],
+    followers_order=FollowersOrder.ASC
+)
